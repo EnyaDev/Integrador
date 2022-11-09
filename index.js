0a5fd3ed38acb43705058ea1d0b900cc3a23d115
EnyaDev <enyam.rs@gmail.com>
2022-11-08 20:09:04 -0600

    Objects, arrays y f superiores

--- index.js
+++ index.js
@@ -2,24 +2,48 @@ function bye(){
     console.log ("Gracias, te esperamos!")
 }
 
-let producto = parseInt(prompt('Escoge que te gustaria llevar en tus uñas hoy: 1. De Colores - 2.Efecto - 3.Glitter - 4.Encapsulado - 5.Accesorio 3D'))
+let producto = parseInt(prompt('Escoge que diseño te gustaria llevar en tus uñas: 1. De Colores  2.Efecto - 3.Glitter - 4.Encapsulado - 5.Accesorio 3D'))
 let seguirComprando = true
 let totalCompra = 0.00
 let decision
-while (seguirComprando === true) {
-    if (producto === 1) {
-        totalCompra = totalCompra + 10.00
-    } else if (producto === 2) {
-        totalCompra = totalCompra + 40.00
-    } else if (producto === 3) {
-        totalCompra = totalCompra + 35.00
-    } else if (producto === 4) {
-        totalCompra = totalCompra + 100.00
-    } else if (producto === 5) {
-        totalCompra = totalCompra + 150.00
-    }
 
-    decision = parseInt(prompt('Quieres seguir escogiendo? 1.Sii = 2.Nop'))
+const diseñosArray = []
+
+class NewDesign {
+    constructor(id,tipo,price,stock) {
+        this.id = id
+        this.tipo = tipo
+        this.price = price
+        this.stock = stock
+    }
+}
+const deColores = new NewDesign(1,'De Colores', 10.0, 10)
+diseñosArray.push(deColores)
+const efecto = new NewDesign(2,'Efecto', 40.0, 10)
+diseñosArray.push(efecto)
+const glitter = new NewDesign(3,'Glitter', 35.0, 10)
+diseñosArray.push(glitter)
+const encapsulado = new NewDesign(4,'Encapsulado', 100.0, 10)
+diseñosArray.push(encapsulado)
+const accesorio3d = new NewDesign(5,'Accesorio 3D', 150.0, 10)
+diseñosArray.push(accesorio3d);
+console.log(diseñosArray)
+
+while (seguirComprando === true) {
+    // if (producto === 1) {
+    //     totalCompra = totalCompra + 10.00
+    // } else if (producto === 2) {
+    //     totalCompra = totalCompra + 40.00
+    // } else if (producto === 3) {
+    //     totalCompra = totalCompra + 35.00
+    // } else if (producto === 4) {
+    //     totalCompra = totalCompra + 100.00
+    // } else if (producto === 5) {
+    //     totalCompra = totalCompra + 150.00
+    // }
+    totalCompra = totalCompra + diseñosArray[producto-1].price
+    
+    decision = parseInt(prompt('Quieres seguir escogiendo? 1.Si = 2.No'))
     if(decision===1) {
         producto = parseInt(prompt('Escoge que te gustaria llevar en tus uñas hoy: 1. De Colores - 2.Efecto - 3.Glitter - 4.Encapsulado - 5.Accesorio 3D'))
     } else {
