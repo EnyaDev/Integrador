700bda7eb7ca20811e5d69d9313ca188b17ef475
EnyaDev <enyam.rs@gmail.com>
2022-12-30 00:20:06 -0600

    Corrigiendo fetch,async

--- index.js
+++ index.js
@@ -1,114 +1,178 @@
 const divCards = document.querySelector('.cards')
 const lista = document.querySelector('#lista')
-const formUser = document.getElementById('form')
-const botonFilter = document.querySelector('#filtrar')
+// const formUser = document.getElementById('form')
+// const botonFilter = document.querySelector('#filtrar')
 
 
-const showCat = async () => {
-    const categoriesFetch = await fetch('categories.json')
-    const categoriesJson = await categoriesFetch.json()
-    categoriesJson.forEach((cat) => {
-        const option = document.createElement('option')
-        option.innerText = `${cat}`
-        lista.append(option)
-    })
+// const showCat = async () => {
+//     const categoriesFetch = await fetch('categories.json')
+//     const categoriesJson = await categoriesFetch.json()
+//     categoriesJson.forEach((cat) => {
+//         const option = document.createElement('option')
+//         option.innerText = `${cat}`
+//         lista.append(option)
+//     })
+// }
+
+// const searchAll = async () => {
+//     const disenosFetch = await fetch('diseno.json')
+//     const disenosJson = await disenosFetch.json()
+//     disenosJson.forEach((prod) => {
+//         const { id, name, price, category, image } =
+//             divCards.innerHTML += `
+//     <div class="card">
+//     <img src:"${image} class:"card-img-top">
+//     <div class="card-body">
+//         <h5 class="card-title">${name}</h5>
+//         <p class="card-text">$${price}</p>
+//         <button id="${id}" class="btn btn-primary">AGREGAR</button>
+//     </div>
+//     `
+//     })
+// }
+
+// const searchForCat = async () => {
+//     divCards.innerHTML = ''
+//     const categorySelected = lista.value
+//     const disenosFetch = await fetch('diseno.json')
+//     const disenosJson = await disenosFetch.json()
+
+//     const disenosFiltrados = disenosJson.filter(prod=>prod.category===categorySelected)
+//     disenosFiltrados.forEach((prod) => {
+//         const { id, name, price, category, image } =
+//             divCards.innerHTML += `
+//     <div class="card">
+//     <img src:"${image} class:"card-img-top">
+//     <div class="card-body">
+//         <h5 class="card-title">${name}</h5>
+//         <p class="card-text">$${price}</p>
+//         <button id="${id}" class="btn btn-primary">AGREGAR</button>
+//     </div>
+//     `
+//     })
+// }
+
+// searchAll()
+// showCat()
+// botonFiltrar.onclick = searchForCat
+
+//     // const diseñosArray = []
+//     // class NewDesign {
+
+//     //     constructor(id, name, price, image) {
+//     //         this.id = id
+//     //         this.name = name
+//     //         this.price = price
+//     //         this.stock = stock
+//     //         this.image = image
+//     //     }
+//     // } 
+
+//     //DOM
+//     diseñosArray.forEach((elemento) => {
+//         selectDesign.append(optionDesign)
+//     })
+
+//     //funciones
+
+//     const buscarDesign = async () => {
+//         const designFetch = await fetch()
+//         const designJson = await designFetch.json(disenos.json)
+//         designJson.forEach(prod => {
+//             const { id, name, price, image } = prod
+//         })
+//     }
+
+//     // buscar elementos al DOM
+
+//     const divProductos = document.querySelector('#divProductos')
+
+//     diseñosArray.forEach((producto) => {
+//         divCards.innerHTML += `
+//     <div id="${producto.id}" class="card cardDesign">
+//     <div class="card-body">
+//     <h5 class="card-title">${producto.nombre}</h5>
+//     <p class="card-text">$${producto.precio}</p>
+//     <button id="${producto.id}" class="btn btn-primary">AGREGAR</button>
+//     </div>
+//     </div>
+
+//     `
+//     })
+
+
+//     //carrito
+//     const carrito = []
+
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const prodContainer = document.querySelector('.prod-container');
+
+
+//funciones
+function searchProd() {
+    fetch('diseno.json')
+        .then(res => res.json())
+        .then((data) => {
+            searchAll(data)
+        });
 }
+
+searchProd();
 
 const searchAll = async () => {
     const disenosFetch = await fetch('diseno.json')
-    const disenosJson = await disenosFetch.json()
-    disenosJson.forEach((prod) => {
-        const { id, name, price, category, image } =
-            divCards.innerHTML += `
-    <div class="card">
-    <img src:"${image} class:"card-img-top">
-    <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">$${price}</p>
-        <button id="${id}" class="btn btn-primary">AGREGAR</button>
-    </div>
-    `
-    })
+    console.log(disenosFetch);
+
+// Si l respuesta es correcta
+if(disenosFetch.status === 200) {
+    const datos = await disenosFetch.json();
+
+datos.forEach(prod => {
+    console.log(prod);
+});
+
+} else if (disenosFetch.status === 404);
 }
 
-const searchForCat = async () => {
-    divCards.innerHTML = ''
-    const categorySelected = lista.value
-    const disenosFetch = await fetch('diseno.json')
-    const disenosJson = await disenosFetch.json()
+searchAll();
 
-    const disenosFiltrados = disenosJson.filter(prod=>prod.category===categorySelected)
-    disenosFiltrados.forEach((prod) => {
-        const { id, name, price, category, image } =
-            divCards.innerHTML += `
-    <div class="card">
-    <img src:"${image} class:"card-img-top">
-    <div class="card-body">
-        <h5 class="card-title">${name}</h5>
-        <p class="card-text">$${price}</p>
-        <button id="${id}" class="btn btn-primary">AGREGAR</button>
-    </div>
-    `
-    })
-}
+// function openDesign(prod) {
+//     const designJson = document.createElement('diseno.json');
+//     designJson.forEach(prod => {
+//         const { id, name, price, image } = prod
+//     })
+// }
 
-searchAll()
-showCat()
-botonFiltrar.onclick = searchForCat
-
-    // const diseñosArray = []
-    // class NewDesign {
-
-    //     constructor(id, name, price, image) {
-    //         this.id = id
-    //         this.name = name
-    //         this.price = price
-    //         this.stock = stock
-    //         this.image = image
-    //     }
-    // } 
-
-    // const cromado = new NewDesign(1, 'Cromado', 40.0, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmeNRLFV4TPbGTHttoqZRdZu6PHACze6-87RwvHDBT1XqTV-hrBOZp3ZvfoEa9wyBRfl4&usqp=CAU')
-    // diseñosArray.push(cromado)
-    // const glitter = new NewDesign(2, 'Glitter', 35.0, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTax8E5wcLwIdqVhPOqG69WykMfWBMwED8OQPIhg85x6uRBLwUC5NYkuuCDweSm2r1Pd8Q&usqp=CAU')
-    // diseñosArray.push(glitter)
-    // const encapsulado = new NewDesign(3, 'Encapsulado', 100.0, 'https://i.blogs.es/42d526/274857342_3035266973379831_8363064571846029922_n/450_1000.jpeg')
-    // diseñosArray.push(encapsulado)
-    // const diseño3d = new NewDesign(4, 'Diseño 3D', 150.0, 'https://heraldodemexico.com.mx/u/fotografias/m/2022/6/8/f768x1-525121_525248_5050.jpeg')
-    // diseñosArray.push(diseño3d)
-
-    //DOM
-    diseñosArray.forEach((elemento) => {
-        selectDesign.append(optionDesign)
-    })
-
-    //funciones
-
-    const buscarDesign = async () => {
-        const designFetch = await fetch()
-        const designJson = await designFetch.json(disenos.json)
-        designJson.forEach(prod => {
-            const { id, name, price, image } = prod
-        })
-    }
-
-    // buscar elementos al DOM
-
-    const divProductos = document.querySelector('#divProductos')
-
-    diseñosArray.forEach((producto) => {
-        divCards.innerHTML += `
-    <div id="${producto.id}" class="card cardDesign">
-    <div class="card-body">
-    <h5 class="card-title">${producto.nombre}</h5>
-    <p class="card-text">$${producto.precio}</p>
-    <button id="${producto.id}" class="btn btn-primary">AGREGAR</button>
-    </div>
-    </div>
-    
-    `
-    })
+//     const buscarDesign = async () => {
+//         const designFetch = await fetch()
+//         const designJson = await designFetch.json(disenos.json)
+//         designJson.forEach(prod => {
+//             const { id, name, price, image } = prod
+//         })
+//
+//
+//{
+// const card = async () => {
+//     const designFetch = await fetch()
+//     const designJson = await designFetch.json(disenos.json)
+//     designJson.forEach(prod => {
+//         const { id, name, price, image } = prod
+//     })
+// } //
 
 
-    //carrito
-    const carrito = []
+
+// const disenosJson = await disenosFetch.json()
+//             disenosJson.forEach((prod) => {
+//                const { id, name, price, image } =
+//                      divCards.innerHTML += `
+//             <div class="card">
+//              <img src:"${image}">
+//              <div class="card-body">
+//                  <h5 class="card-title">${name}</h5>
+//                 <p class="card-text">$${price}</p>
+//                  <button id="${id}" class="btn btn-primary">AGREGAR</button>
+//              </div>
+//              `
+//              })
