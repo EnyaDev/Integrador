b42a6b23c3cf8cea03b80ea9ddaca0d9ce7c2aab
EnyaDev <enyam.rs@gmail.com>
2022-12-30 23:25:09 -0600

    correcciones fetch,librerias,async,vista web

--- index.js
+++ index.js
@@ -1,117 +1,27 @@
-const divCards = document.querySelector('.cards')
+const designArray = []
+const formUser = document.getElementById('form')
 const lista = document.querySelector('#lista')
-// const formUser = document.getElementById('form')
-// const botonFilter = document.querySelector('#filtrar')
 
 
-// const showCat = async () => {
-//     const categoriesFetch = await fetch('categories.json')
-//     const categoriesJson = await categoriesFetch.json()
-//     categoriesJson.forEach((cat) => {
-//         const option = document.createElement('option')
-//         option.innerText = `${cat}`
-//         lista.append(option)
-//     })
-// }
+class NewDesign {
+    constructor(name, price, stock) {
+        this.name = name
+        this.price = price
+        this.stock = stock
+    }
+}
+const cromado = new NewDesign('Cromado', 40.0, 10)
+designArray.push(cromado)
+const glitter = new NewDesign('Glitter', 35.0, 10)
+designArray.push(glitter)
+const encapsulado = new NewDesign('Encapsulado', 100.0, 10)
+designArray.push(encapsulado)
+const diseño3d = new NewDesign('Diseño 3D', 150.0, 10)
+designArray.push(diseño3d)
 
-// const searchAll = async () => {
-//     const disenosFetch = await fetch('diseno.json')
-//     const disenosJson = await disenosFetch.json()
-//     disenosJson.forEach((prod) => {
-//         const { id, name, price, category, image } =
-//             divCards.innerHTML += `
-//     <div class="card">
-//     <img src:"${image} class:"card-img-top">
-//     <div class="card-body">
-//         <h5 class="card-title">${name}</h5>
-//         <p class="card-text">$${price}</p>
-//         <button id="${id}" class="btn btn-primary">AGREGAR</button>
-//     </div>
-//     `
-//     })
-// }
-
-// const searchForCat = async () => {
-//     divCards.innerHTML = ''
-//     const categorySelected = lista.value
-//     const disenosFetch = await fetch('diseno.json')
-//     const disenosJson = await disenosFetch.json()
-
-//     const disenosFiltrados = disenosJson.filter(prod=>prod.category===categorySelected)
-//     disenosFiltrados.forEach((prod) => {
-//         const { id, name, price, category, image } =
-//             divCards.innerHTML += `
-//     <div class="card">
-//     <img src:"${image} class:"card-img-top">
-//     <div class="card-body">
-//         <h5 class="card-title">${name}</h5>
-//         <p class="card-text">$${price}</p>
-//         <button id="${id}" class="btn btn-primary">AGREGAR</button>
-//     </div>
-//     `
-//     })
-// }
-
-// searchAll()
-// showCat()
-// botonFiltrar.onclick = searchForCat
-
-//     // const diseñosArray = []
-//     // class NewDesign {
-
-//     //     constructor(id, name, price, image) {
-//     //         this.id = id
-//     //         this.name = name
-//     //         this.price = price
-//     //         this.stock = stock
-//     //         this.image = image
-//     //     }
-//     // } 
-
-//     //DOM
-//     diseñosArray.forEach((elemento) => {
-//         selectDesign.append(optionDesign)
-//     })
-
-//     //funciones
-
-//     const buscarDesign = async () => {
-//         const designFetch = await fetch()
-//         const designJson = await designFetch.json(disenos.json)
-//         designJson.forEach(prod => {
-//             const { id, name, price, image } = prod
-//         })
-//     }
-
-//     // buscar elementos al DOM
-
-//     const divProductos = document.querySelector('#divProductos')
-
-//     diseñosArray.forEach((producto) => {
-//         divCards.innerHTML += `
-//     <div id="${producto.id}" class="card cardDesign">
-//     <div class="card-body">
-//     <h5 class="card-title">${producto.nombre}</h5>
-//     <p class="card-text">$${producto.precio}</p>
-//     <button id="${producto.id}" class="btn btn-primary">AGREGAR</button>
-//     </div>
-//     </div>
-
-//     `
-//     })
-
-
-//     //carrito
-//     const carrito = []
-
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const prodContainer = document.querySelector('.prod-container');
-
-
-//funciones
+// funciones
 function searchProd() {
-    fetch('diseno.json')
+    fetch('/design.json')
         .then(res => res.json())
         .then((data) => {
             searchAll(data)
@@ -121,58 +31,88 @@ function searchProd() {
 searchProd();
 
 const searchAll = async () => {
-    const disenosFetch = await fetch('diseno.json')
+    const disenosFetch = await fetch('/design.json')
     console.log(disenosFetch);
 
 // Si l respuesta es correcta
 if(disenosFetch.status === 200) {
     const datos = await disenosFetch.json();
 
-datos.forEach(prod => {
-    console.log(prod);
-});
-
-} else if (disenosFetch.status === 404);
+// let nailDesign = '';
+// designArray.forEach(design => {
+//        cards.innerHTML += `
+//      <div id="${design.id}" class="cards">
+//       <div class="card-body">
+//      <h5 class="card-title">${design.nombre}</h5>
+//     <p class="card-text">$${design.precio}</p>
+//     <button id="${design.id}" class="btn btn-primary">AGREGAR</button>
+//   </div>
+//      </div>
+//       `
+//       })
+    } else if (disenosFetch.status === 404);
 }
 
-searchAll();
+//DOM
+const selectDesign = document.getElementById('lista')
+designArray.forEach((elemento) => {
+    const optionDesign = document.createElement('option')
+    optionDesign.innerText = `${elemento.name}: $${elemento.price}`
+    optionDesign.setAttribute('id', `${elemento.id}`)
+    selectDesign.append(optionDesign)
+})
 
-// function openDesign(prod) {
-//     const designJson = document.createElement('diseno.json');
-//     designJson.forEach(prod => {
-//         const { id, name, price, image } = prod
-//     })
-// }
+//carrito
+const carrito = []
 
-//     const buscarDesign = async () => {
-//         const designFetch = await fetch()
-//         const designJson = await designFetch.json(disenos.json)
-//         designJson.forEach(prod => {
-//             const { id, name, price, image } = prod
-//         })
-//
-//
-//{
-// const card = async () => {
-//     const designFetch = await fetch()
-//     const designJson = await designFetch.json(disenos.json)
-//     designJson.forEach(prod => {
-//         const { id, name, price, image } = prod
-//     })
-// } //
+const botonAgregar = document.getElementById('botonAdd')
+const finalizarCompra = document.getElementById('finalizarCompra')
 
+botonAdd.onclick = () => {
+    const indexDesign = selectDesign.selectedIndex
+    const diseñoSeleccionado = designArray[indexDesign]
+    carrito.push(diseñoSeleccionado)
+}
 
+finalizarCompra.onclick = () => {
+    let total = 0
+    carrito.forEach((design) => {
+        total = total + design.price
+    })
+    Swal.fire(`El total es de ${total} dolares. Ingresa tus datos para completar tu cita`)
+}
 
-// const disenosJson = await disenosFetch.json()
-//             disenosJson.forEach((prod) => {
-//                const { id, name, price, image } =
-//                      divCards.innerHTML += `
-//             <div class="card">
-//              <img src:"${image}">
-//              <div class="card-body">
-//                  <h5 class="card-title">${name}</h5>
-//                 <p class="card-text">$${price}</p>
-//                  <button id="${id}" class="btn btn-primary">AGREGAR</button>
-//              </div>
-//              `
-//              })
+//elemento
+const inputNombre = document.getElementById('nombre')
+const inputApellido = document.getElementById('apellido')
+const usuario = {}
+
+inputNombre.onchange = (e) => {
+    usuario.nombre = e.target.value
+}
+inputApellido.onchange = (e) => {
+    usuario.apellido = e.target.value
+}
+
+const infoUser = {}
+
+formUser.onsubmit = (e) => {
+    e.preventDefault()
+    infoUser.nombre = inputNombre.value
+    infoUser.apellido = inputApellido.value
+    localStorage.setItem('infoUser',JSON.stringify(infoUser))
+}
+
+const infoUserStorage = JSON.parse(localStorage.getItem('infoUser'))
+
+if(infoUserStorage.nombre !== "" || infoUserStorage.apellido !== ""){
+    welcome.innerText = `Bienvenida de vuelta ${infoUserStorage.nombre} ${infoUserStorage.apellido}!`
+}
+
+agendar.onclick = () => {
+    Swal.fire({
+    text: `Gracias por agendar con nosotras ${usuario.nombre} ${usuario.apellido} te esperamos`,
+    icon: 'success',
+})
+}
+
